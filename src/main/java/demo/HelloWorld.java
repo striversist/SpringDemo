a49c8dc0f4c1486f71c4e62b5db1fb74cb545d7a
aarontang <aarontang@tencent.com>
2016-03-03 16:28:31 +0800

    [hello]增加HelloWorld, MainApp & Beans.xml

---
+++ src/main/java/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class HelloWorld {
+
+    private String message;
+
+    public void getMessage() {
+        System.out.println("Your message: " + message);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
