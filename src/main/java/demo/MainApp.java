dc760eb6c2610565aaa91393f36ff2888db533ca
aarontang <aarontang@tencent.com>
2016-03-04 11:18:52 +0800

    [di]展示带参数的构造函数的DI用法（关键：bean constructor-arg，参考：http://www.tutorialspoint.com/spring/constructor_based_dependency_injection.htm）

--- src/main/java/demo/MainApp.java
+++ src/main/java/demo/MainApp.java
@@ -3,6 +3,8 @@ package demo;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import demo.di.TextEditor;
+
 public class MainApp {
 
     public static void main(String[] args) {
@@ -16,6 +18,9 @@ public class MainApp {
         obj2.getMessage1();
         obj2.getMessage2();
         obj2.getMessage3();
+
+        TextEditor textEditor = (TextEditor) context.getBean("text_editor");
+        textEditor.checkSpelling();
         
         context.registerShutdownHook();
     }
---
+++ src/main/java/demo/di/TextEditor.java
@@ -0,0 +1,14 @@
+package demo.di;
+
+public class TextEditor {
+
+    private SpellChecker spellChecker;
+
+    public TextEditor(SpellChecker spellChecker) {
+        this.spellChecker = spellChecker;
+    }
+    
+    public void checkSpelling() {
+        spellChecker.checkSpelling();
+    }
+}
