db5f16d11db1248ef39c454709d13472883bffec
aarontang <aarontang@tencent.com>
2016-03-04 10:49:56 +0800

    [inheritance]展示bean parent属性的用法（另：可定义template: <bean id="beanTeamplate" abstract="true">...</bean>，让其他bean继承）

--- src/main/java/demo/HelloChina.java
+++ src/main/java/demo/HelloChina.java
@@ -1,10 +1,11 @@
 package demo;
 
-public class HelloWorld {
+public class HelloChina {
 
     private String message1;
     private String message2;
-
+    private String message3;
+    
     public void init() {
         System.out.println("Bean " + getClass().getSimpleName() + " is going though init.");
     }
@@ -12,15 +13,16 @@ public class HelloWorld {
     public void destroy() {
         System.out.println("Bean " + getClass().getSimpleName() + " will destroy now.");
     }
-    
-    public void getMessage1() {
+
+    public String getMessage1() {
         System.out.println("Your message1: " + message1);
+        return message1;
     }
 
     public void setMessage1(String message1) {
         this.message1 = message1;
     }
-    
+
     public String getMessage2() {
         System.out.println("Your message2: " + message2);
         return message2;
@@ -29,4 +31,14 @@ public class HelloWorld {
     public void setMessage2(String message2) {
         this.message2 = message2;
     }
+
+    public String getMessage3() {
+        System.out.println("Your message3: " + message3);
+        return message3;
+    }
+
+    public void setMessage3(String message3) {
+        this.message3 = message3;
+    }
+
 }
--- src/main/java/demo/MainApp.java
+++ src/main/java/demo/MainApp.java
@@ -8,8 +8,14 @@ public class MainApp {
     public static void main(String[] args) {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         
-        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-        obj.getMessage();
+        HelloWorld obj1 = (HelloWorld) context.getBean("hello_world");
+        obj1.getMessage1();
+        obj1.getMessage2();
+        
+        HelloChina obj2 = (HelloChina) context.getBean("hello_china");
+        obj2.getMessage1();
+        obj2.getMessage2();
+        obj2.getMessage3();
         
         context.registerShutdownHook();
     }
